52c58afd075908e76b4fb176dd9cf2ac9789e439
Lawrence R. Thomas III <lathomas@sas.upenn.edu>
2014-02-03 23:28:17 -0500

    google commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GooglePage::Application.config.secret_key_base = '457889fe2dfdbf92bc6053311f8469b4058dd42cc78bf02969c3da9063c63807075cfb9b3adfb62d723b56daba6bc375932ea8e14434c12f471b432080f6a189'
